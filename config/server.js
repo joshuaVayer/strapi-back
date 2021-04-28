a28f6bf4ed8684063031801c5ea5dff627c6db8c
Joshua Vayer <joshua.vayer@outlook.com>
2021-04-28 13:23:06 +0200

    First project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f872e73550d56deedb1c243484d7db52'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7b5a077b-73aa-4862-9ecf-4c2c950ffd13'
+};
